fix electrode being in wrong order

inaivu.py
@@ -2,9 +2,10 @@ from __future__ import division
 import os
 import numpy as np
 from traits.api import (HasTraits, Any, Dict, Instance, Str, Float,
-    Range, on_trait_change, File, Button, Int)
+    Range, on_trait_change, File, Button, Int, Bool, Enum)
 from traitsui.api import (View, Item, Group, OKCancelButtons, ShellEditor,
-    HGroup, VGroup, Handler, RangeEditor)
+    HGroup, VGroup, Handler, RangeEditor, Action, CancelButton, Handler,
+    NullEditor)
 from traitsui.message import error as error_dialog
 
 import mne
@@ -16,11 +17,12 @@ from collections import OrderedDict
 
 import source_signal
 
-class InaivuModel(HasTraits):
+class InaivuModel(Handler):
 
     brain = Any # Instance(surfer.viz.Brain)
     ieeg_loc = Any #OrderedDict
     ieeg_glyph = Any # Instance(mlab.Glyph3D)
+    ch_names = Any #List(Str) ? 
 
     scene = Any # mayavi.core.Scene
     scene = Instance(MlabSceneModel, ())
@@ -42,6 +44,7 @@ class InaivuModel(HasTraits):
 
     opacity = Float(.35)
 
+    use_smoothing = Bool(False)
     smoothing_steps = Int(0)
 
     smoothl = Any #Either(np.ndarray, None)
@@ -50,7 +53,26 @@ class InaivuModel(HasTraits):
     browser = Any #Instance(BrowseStc)
 
     current_script_file = File
-    run_script_button = Button('Run')
+    run_script_button = Button('Run script')
+
+
+    # movie window
+    make_movie_button = Button('Movie')
+
+    movie_filename = File
+    movie_normalization_style = Enum('local', 'global', 'none')
+
+    movie_tmin = Float(0.)
+    movie_tmax = Float(1.)
+
+    movie_framerate = Float(24)
+    movie_dilation = Float(2)
+    movie_bitrate = Str('750k')
+    movie_interpolation = Enum('quadratic', 'cubic', 'linear', 'slinear',
+        'nearest', 'zero')
+    movie_animation_degrees = Float(0.)
+
+    OKMakeMovieAction = Action(name='Make movie', action='do_movie')
 
     traits_view = View(
         Item('scene', editor=SceneEditor(scene_class=MayaviScene),
@@ -62,8 +84,9 @@ class InaivuModel(HasTraits):
                 label='time'),
         ),
         HGroup(
+            Item('make_movie_button', show_label=False),
             Item('current_script_file'),
-            Item('run_script_button'),
+            Item('run_script_button', show_label=False),
         ),
         #Item('time_slider', style='custom', show_label=False),
         Item('shell', editor=ShellEditor(), height=300, show_label=False),
@@ -75,6 +98,62 @@ class InaivuModel(HasTraits):
     def _run_script_button_fired(self):
         with open(self.current_script_file) as fd:
             exec(fd)
+
+    make_movie_view = View(
+        Item('movie_filename', label='filename', style='readonly'),
+        HGroup(
+            VGroup(
+                Item('movie_tmin', label='tmin'),
+                Item('movie_framerate', label='framerate'),
+            ),
+            VGroup(
+                Item('movie_tmax', label='tmax'),
+                Item('movie_bitrate', label='bitrate'),
+                Item('movie_interpolation', label='interp'),
+            ),
+            VGroup(
+                Item('movie_dilation', label='temporal dilation'),
+                Item('movie_animation_degrees', label='degrees rotate'),
+                Item('movie_normalization_style', label='normalization'),
+            ),
+        ),
+
+#        HGroup(
+#            Item('movie_tmin', label='tmin'),
+#            Item('movie_tmax', label='tmax'),
+#            Item('movie_dilation', label='temporal dilation'),
+#        ),
+#        HGroup(
+#            Item('movie_framerate', label='framerate'),
+#            Item('movie_bitrate', label='bitrate (b/s)'),
+#            Item('movie_interpolation', label='interp'),
+#        ),
+        title='Chimer exodus from Aldmeris',
+        buttons=[OKMakeMovieAction, CancelButton],
+    )
+
+    def _make_movie_button_fired(self):
+        self.edit_traits(view='make_movie_view')
+
+    def do_movie(self, info):
+        from pyface.api import FileDialog, OK as FileOK
+        dialog = FileDialog(action='save as')
+        dialog.open()
+        if dialog.return_code != FileOK:
+            return
+
+        self.movie_filename = os.path.join(dialog.directory, dialog.filename)
+        info.ui.dispose()
+        self.movie( self.movie_filename, 
+            tmin=self.movie_tmin,
+            tmax=self.movie_tmax,
+            normalization=self.movie_normalization_style,
+            framerate=self.movie_framerate,
+            dilation=self.movie_dilation,
+            bitrate=self.movie_bitrate,
+            interpolation=self.movie_interpolation,
+            animation_degrees=self.movie_animation_degrees,
+            )
 
     def build_surface(self, subjects_dir=None, subject=None):
         '''
@@ -150,7 +229,6 @@ class InaivuModel(HasTraits):
                 ra.info['chs'][i]['kind'] == mne.io.constants.FIFF.FIFFV_SEEG_CH]
 
             self.ch_names = [e[0] for e in elecs]
-            self.ieeg_loc = dict(elecs)
 
             locs = np.array([e[1] for e in elecs])
 
@@ -160,7 +238,6 @@ class InaivuModel(HasTraits):
             self.ch_names = sfp.ch_names
 
             elecs = [(name, loc) for name, loc in zip(self.ch_names, locs)]
-            self.ieeg_loc = dict(elecs)
 
         else:
             locs = np.array(elec_locs)
@@ -169,8 +246,9 @@ class InaivuModel(HasTraits):
 
             elecs = [(name, loc) for name, loc in zip(ch_names, locs)]
 
-            self.ieeg_loc = dict(elecs)
+        # compare signal.ch_names to the ch_names here
 
+        self.ieeg_loc = dict(elecs)
 
         source = mlab.pipeline.scalar_scatter(locs[:,0], locs[:,1], locs[:,2],
             figure=self.scene.mayavi_scene)
@@ -313,33 +391,47 @@ class InaivuModel(HasTraits):
             
     
     def add_invasive_signal(self, name, signal):
-        #self.invasive_signals.append(signal)
-        self.invasive_signals[name] = signal
-        self.current_invasive_signal = signal
+        if len(self.ch_names) == 0:
+            raise ValueError("Cannot add invasive signal without first "
+                "specifying order of invasive electrodes")
 
-        stc = signal.mne_source_estimate
+        self.invasive_signals[name] = signal
+        self.set_current_invasive_signal(name)
+
+    def set_current_invasive_signal(self, name):
+        self.current_invasive_signal = sig = self.invasive_signals[name]
+
+        stc = sig.mne_source_estimate
 
         if stc.times[0] < self._time_low:
             self._time_low = stc.times[0]
         if stc.times[-1] > self._time_high:
             self._time_high = stc.times[-1]
 
-    def set_current_invasive_signal(self, name):
-        self.current_invasive_signal = self.invasive_signals[name]
+        #reorder signal in terms of self.ch_names
+        reorder_map = source_signal.adj_sort( sig.ch_names, self.ch_names )
+        reorder_signal = stc.data[reorder_map]
+        #ignore vertices, we only ever bother to get stc.data
+        #reorder_vertices = stc.vertno[???]
+
+        sig.ch_names = self.ch_names
+        sig.data = reorder_signal
 
     def add_noninvasive_signal(self, name, signal):
         self.noninvasive_signals[name] = signal
-        self.current_noninvasive_signal = signal
+        self.set_current_noninvasive_signal(name)
 
-        stc = signal.mne_source_estimate
+    def set_current_noninvasive_signal(self, name):
+        self.current_noninvasive_signal = sig = self.noninvasive_signals[name]
+
+        stc = sig.mne_source_estimate
 
         if stc.times[0] < self._time_low:
             self._time_low = stc.times[0]
         if stc.times[-1] > self._time_high:
             self._time_high = stc.times[-1]
 
-    def set_current_noninvasive_signal(self, name):
-        self.current_noninvasive_signal = self.noninvasive_signals[name]
+        sig.data = stc.data
 
     def _display_interpolated_invasive_signal_timepoint(self, idx, ifunc):
         scalars = ifunc(idx)
@@ -347,6 +439,7 @@ class InaivuModel(HasTraits):
         self.ieeg_glyph.mlab_source.dataset.point_data.scalars = (
             np.array(scalars))
         self.ieeg_glyph.actor.mapper.scalar_visibility = True
+        self.ieeg_glyph.module_manager.scalar_lut_manager.data_range = (0,1)
 
     def _display_interpolated_noninvasive_signal_timepoint(self, idx, ifunc, 
             interpolation='quadratic'):
@@ -356,72 +449,98 @@ class InaivuModel(HasTraits):
         rvt = self.current_noninvasive_signal.mne_source_estimate.rh_vertno
 
         if len(lvt) > 0:
-            lh_scalar = scalars[lvt]
+            #assumes all lh scalar precede all rh scalar
+            #if not, then we need separate lh_ifunc and rh_ifunc for this case
+            lh_scalar = scalars[len(lvt):]
+            #lh_scalar = scalars[lvt]
             lh_surf = self.brain.brains[0]._geo_surf
-            if len(lvt) < len(self.brain.geo['lh'].coords):
-                lh_scalar = self.smoothl * lh_scalar
+            if len(lvt) < len(self.brain.geo['lh'].coords): 
+                if self.smoothing_steps > 0:
+                    lh_scalar = self.smoothl * lh_scalar
+                else:
+                    ls = np.ones( len(self.brain.geo['lh'].coords))*.5
+                    ls[rvt] = lh_scalar
+                    lh_scalar = ls
 
             lh_surf.mlab_source.scalars = lh_scalar
+            lh_surf.module_manager.scalar_lut_manager.data_range = (0,1)
 
         if len(rvt) > 0:
-            rh_scalar = scalars[rvt]
+            rh_scalar = scalars[:len(rvt)]
+            #rh_scalar = scalars[rvt]
             rh_surf = self.brain.brains[1]._geo_surf
             if len(rvt) < len(self.brain.geo['rh'].coords):
-                rh_scalar = self.smoothr * rh_scalar
+                if self.smoothing_steps > 0:
+                    rh_scalar = self.smoothr * rh_scalar
+                else:
+                    rs = np.ones( len(self.brain.geo['rh'].coords))*.5
+                    rs[rvt] = rh_scalar
+                    rh_scalar = rs
 
             rh_surf.mlab_source.scalars = rh_scalar
+            rh_surf.module_manager.scalar_lut_manager.data_range = (0,1)
 
         #self.brain.set_data_time_index(idx, interpolation)
 
     @on_trait_change('time_slider')
     def _show_closest_timepoint_listen(self):
         self.set_closest_timepoint(self.time_slider)
+        self._force_render()
 
     def set_closest_timepoint(self, time, invasive=True, noninvasive=True):
         if noninvasive:
-            self._set_noninvasive_timepoint(time, 
-                smoothing_steps=self.smoothing_steps)
+            self._set_noninvasive_timepoint(time)
         if invasive:
             self._set_invasive_timepoint(time)
 
     def _set_invasive_timepoint(self, t, normalization='global'):
         if self.current_invasive_signal is None:
             return
-        stc = self.current_invasive_signal.mne_source_estimate
+        sig = self.current_invasive_signal
+        stc = sig.mne_source_estimate
 
         sample_time = np.argmin(np.abs(stc.times - t))
 
         if normalization=='global':
-            dmax = np.max(stc.data)
-            dmin = np.min(stc.data)
-            data = (stc.data-dmin) / (dmax-dmin)
+            dmax = np.max(sig.data)
+            dmin = np.min(sig.data)
+            data = (sig.data-dmin) / (dmax-dmin)
         else:
-            data = stc.data
+            data = sig.data
 
         scalars = data[:,sample_time]
+
+        #from PyQt4.QtCore import pyqtRemoveInputHook
+        #import pdb
+        #pyqtRemoveInputHook()
+        #pdb.set_trace()
 
         self.ieeg_glyph.mlab_source.dataset.point_data.scalars = (
             np.array(scalars))
         self.ieeg_glyph.actor.mapper.scalar_visibility = True
+        self.ieeg_glyph.module_manager.scalar_lut_manager.data_range = (0,1)
 
-    def _setup_noninvasive_viz(self, smoothing_steps=20):
+    def _setup_noninvasive_viz(self):
         lvt = self.current_noninvasive_signal.mne_source_estimate.lh_vertno
         rvt = self.current_noninvasive_signal.mne_source_estimate.rh_vertno
 
-        if 0 < len(lvt) < len(self.brain.geo['lh'].coords):
+        if (0 < len(lvt) < len(self.brain.geo['lh'].coords) and
+                self.smoothing_steps > 0):
             ladj = surfer.utils.mesh_edges(self.brain.geo['lh'].faces)
             self.smoothl = surfer.utils.smoothing_matrix(lvt, ladj, 
-                smoothing_steps)
+                self.smoothing_steps)
 
-        if 0 < len(rvt) < len(self.brain.geo['rh'].coords):
+        if (0 < len(rvt) < len(self.brain.geo['rh'].coords) and
+                self.smoothing_steps > 0):
             radj = surfer.utils.mesh_edges(self.brain.geo['lh'].faces)
             self.smoothr = surfer.utils.smoothing_matrix(rvt, radj,
-                smoothing_steps)
+                self.smoothing_steps)
 
         for i,brain in enumerate(self.brain.brains):
 
-            #skip if no vertices in hemisphere
+            #leave gray if no vertices in hemisphere
             if len(lvt)==i==0 or len(rvt)==0==i-1:
+                brain._geo_surf.actor.mapper.scalar_visibility=False
                 continue
 
             brain._geo_surf.module_manager.scalar_lut_manager.lut_mode = (
@@ -430,48 +549,61 @@ class InaivuModel(HasTraits):
                 True)
             brain._geo_surf.actor.mapper.scalar_visibility=True
 
-    def _set_noninvasive_timepoint(self, t, normalization='global',
-            smoothing_steps=20):
+            brain._geo_surf.module_manager.scalar_lut_manager.data_range=(0,1)
+
+    def _set_noninvasive_timepoint(self, t, normalization='global'):
         if self.current_noninvasive_signal is None:
             return
         stc = self.current_noninvasive_signal.mne_source_estimate
 
         sample_time = np.argmin(np.abs(stc.times - t))
 
-        if normalization=='global':
-            dmax = np.max(stc.data)
-            dmin = np.min(stc.data)
-            data = (stc.data-dmin) / (dmax-dmin)
-        else:
-            data = stc.data
-
-        scalars = stc.data[:,sample_time]
-
         lvt = self.current_noninvasive_signal.mne_source_estimate.lh_vertno
         rvt = self.current_noninvasive_signal.mne_source_estimate.rh_vertno
 
-        self._setup_noninvasive_viz(smoothing_steps=20)
+        if normalization=='global':
+            if len(lvt) > 0:
+                lh_dmax = np.max(stc.lh_data)
+                lh_dmin = np.min(stc.lh_data)
+                lh_scalar = (stc.lh_data-lh_dmin) / (lh_dmax-lh_dmin)
+            if len(rvt) > 0:
+                rh_dmax = np.max(stc.rh_data)
+                rh_dmin = np.min(stc.rh_data)
+                rh_scalar = (stc.rh_data-rh_dmin) / (rh_dmax-rh_dmin)
+        else:
+            lh_scalar = stc.lh_data
+            rh_scalar = stc.rh_data
+
+        self._setup_noninvasive_viz()
     
         if len(lvt) > 0:
-            lh_scalar = stc.lh_data[:,sample_time]
-            #lh_scalar = scalars[lvt]
             lh_surf = self.brain.brains[0]._geo_surf
             if len(lvt) < len(self.brain.geo['lh'].coords):
-                lh_scalar = self.smoothl * lh_scalar
+                if self.smoothing_steps > 0:
+                    lh_scalar = self.smoothl * lh_scalar
+                else:
+                    ls = np.array( len(self.brain.geo['lh'].coords))
+                    ls[lvt] = lh_scalar
+                    lh_scalar = ls
             lh_surf.mlab_source.scalars = lh_scalar
+            rh_surf.module_manager.scalar_lut_manager.data_range = (0,1)
 
         if len(rvt) > 0:
-            rh_scalar = stc.rh_data[:,sample_time]
-            #rh_scalar = scalars[rvt]
             rh_surf = self.brain.brains[1]._geo_surf
             if len(rvt) < len(self.brain.geo['rh'].coords):
-                rh_scalar = self.smoothr * rh_scalar
+                if self.smoothing_steps > 0:
+                    rh_scalar = self.smoothr * rh_scalar
+                else:
+                    rs = np.ones( len(self.brain.geo['rh'].coords))*.5
+                    rs[rvt] = rh_scalar
+                    rh_scalar = rs
             rh_surf.mlab_source.scalars = rh_scalar
+            rh_surf.module_manager.scalar_lut_manager.data_range = (0,1)
 
     def movie(self, movname, invasive=True, noninvasive=True,
               framerate=24, interpolation='quadratic', dilation=2,
               tmin=None, tmax=None, normalization='local', debug_labels=False,
-              smoothing_steps=20, bitrate='750k'):
+              bitrate='750k', animation_degrees=0):
         #potentially worth providing different options for normalization and
         #interpolation for noninvasive and invasive data
 
@@ -497,7 +629,7 @@ class InaivuModel(HasTraits):
             lvt = self.current_noninvasive_signal.mne_source_estimate.lh_vertno
             rvt = self.current_noninvasive_signal.mne_source_estimate.rh_vertno
 
-            self._setup_noninvasive_viz(smoothing_steps=20)
+            self._setup_noninvasive_viz()
 
             if len(lvt) > 0:
                 lh_surf = self.brain.brains[0]._geo_surf
@@ -560,6 +692,8 @@ class InaivuModel(HasTraits):
                 nidx = ni_times[i]
                 self._display_interpolated_noninvasive_signal_timepoint(
                     nidx, nfunc, interpolation=interpolation)
+
+            self.scene.camera.azimuth(animation_degrees)
 
             self.scene.render()
             mlab.draw(figure=self.scene.mayavi_scene)
@@ -654,7 +788,7 @@ class InaivuModel(HasTraits):
         all_times = interp1d(raw_sample_times, 
             exact_times)(movie_sample_times)
 
-        data = stc.data[:,smin:smax+1]
+        data = sig.data[:,smin:smax+1]
 
         #print data.shape
         #print all_times.shape
@@ -662,8 +796,8 @@ class InaivuModel(HasTraits):
         if normalization=='none':
             pass
         elif normalization=='conservative':
-            dmax = np.max(stc.data)
-            dmin = np.min(stc.data)
+            dmax = np.max(sig.data)
+            dmin = np.min(sig.data)
             data = (data-dmin) / (dmax-dmin)
         elif normalization=='local': 
             dmax = np.max(data)